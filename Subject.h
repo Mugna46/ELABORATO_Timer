1097656e1510a1509ec6f93ffa0846ca912eaa0d
Mugna46 <69848604+Mugna46@users.noreply.github.com>
2020-09-09 18:04:59 +0200

    Aggiunti Observer e Subject, modificato cmakelists per wxWidgets

---
+++ Subject.h
@@ -0,0 +1,16 @@
+//
+// Created by andre on 09/09/2020.
+//
+
+#ifndef ELABORATO_TIMER_SUBJECT_H
+#define ELABORATO_TIMER_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject{
+    virtual void subscribe(Observer *o)=0;
+    virtual void unsubscribe(Observer *o)=0;
+    virtual void notify()=0;
+};
+
+#endif //ELABORATO_TIMER_SUBJECT_H
